3aaf48b33b15ac493c48ad63a44710facdb86954
kilbil1235 <emmmmanuelchinedu@gmail.com>
2022-09-15 05:56:31 -0700

    Task 0. isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,14 @@
-#include <unistd.h>
+#include "main.h"
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * _isupper - function that checks for uppercase characters
+ * @c: int tyoe number
+ * Return: 1 if uppercase, otherwise 0
  */
-int _putchar(char c)
+int _isupper(int c)
 {
-	return (write(1, &c, 1));
+	if (c > 64 && c < 91)
+		return (1);
+
+	return (0);
 }
