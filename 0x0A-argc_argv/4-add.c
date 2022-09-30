d34cee07fe98ae6c354b6dc10020d5c6ee597b74
kilbil1235 <emmmmanuelchinedu@gmail.com>
2022-09-30 07:17:57 -0700

    Task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,63 +1,15 @@
-i#include <stdio.h>
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * _atoi - converts a string to an integer
- * @s: string to be converted
- *
- * Return: the int converted from the string
- */
-int _atoi(char *s)
-{
-	int i, d, n, len, f, digit;
-
-	i = 0;
-	d = 0;
-	n = 0;
-	len = 0;
-	f = 0;
-	digit = 0;
-
-	while (s[len] != '\0')
-		len++;
-
-	while (i < len && f == 0)
-	{
-		if (s[i] == '-')
-			++d;
-
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			digit = s[i] - '0';
-			if (d % 2)
-				digit = -digit;
-			n = n * 10 + digit;
-			f = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-				break;
-			f = 0;
-		}
-		i++;
-	}
-
-	if (f == 0)
-		return (0);
-
-	return (n);
-}
-
-/**
- * main - adds two positive number
- * @argc: number of arguments
- * @argv: array of arguents
- *
- * Return: 0 (Success), or 1 (Success)
- */
+* main -> this is a function to print its name
+* @argc: argc parameter
+* @argv: an array of a command listed
+* Return: 0 for success
+*/
 int main(int argc, char *argv[])
 {
-	int sum, num, i, j, k;
-
-	sum = 0;
+	int result = 0, num, i, j, k;
 
 	for (i = 1; i < argc; i++)
 	{
@@ -65,21 +17,18 @@ int main(int argc, char *argv[])
 		{
 			if (argv[i][j] > '9' || argv[i][j] < '0')
 			{
-				puts("Error");
+				printf("%s\n", "Error");
 				return (1);
 			}
+
 		}
 	}
 
 	for (k = 1; k < argc; k++)
 	{
-		num = _atoi(argv[k]);
-		if (num >= 0)
-		{
-			sum += num;
-		}
+		num = atoi(argv[k]);
+		result += num;
 	}
-
-	printf("%d\n", sum);
+	printf("%d\n", result);
 	return (0);
 }
